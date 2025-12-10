7533bc0c63aef4bea7bbc9b076218667fcf10bd6
Taylor <huffmantayler@yahoo.com>
2025-12-10 15:45:00 -0500

    commented out unused variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 /// <reference types="vitest" />
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  // const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     resolve: {
